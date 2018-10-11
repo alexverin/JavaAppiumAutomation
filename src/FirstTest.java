970c0a8f10a672d39f2a0485a794ea805270c44b
Aleksey Verin <aleksey.verin@gismart.com>
2018-10-11 15:41:47 +0300

    01. CoreTestCase

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,48 +1,21 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
+public class FirstTest extends CoreTestCase {
 
-    private AppiumDriver driver;
 
-    @Before
-    public void setUp() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("AutomationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "Users/alekseyverin/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
 
     @Test
-    public void firstTest()
+    public void testSearch()
     {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
@@ -170,7 +143,7 @@ public class FirstTest {
     }
 
     @Test
-    public void saveFirstArticleToMyList()
+    public void testSaveFirstArticleToMyList()
     {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
