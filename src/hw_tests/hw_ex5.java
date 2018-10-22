aeb19c43c81cb0dd0efabdf4eb342b4b5ab5ac6c
Aleksey Verin <aleksey.verin@gismart.com>
2018-10-22 15:46:32 +0300

    5.04. Подключение iOS к тестам и первый тест

--- src/hw_tests/hw_ex5.java
+++ src/hw_tests/hw_ex5.java
@@ -3,6 +3,8 @@ package hw_tests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +15,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
 
 public class hw_ex5 {
 
@@ -227,26 +230,23 @@ public class hw_ex5 {
     }
 
 
-    protected void swipeElementToLeft (By by, String error_message)
-    {
+    public void swipeElementToLeft(By by, String error_message) {
         WebElement element = waitForElementPresent(
                 by,
                 error_message,
                 10);
-
-
         int left_x = element.getLocation().getX();
+
         int right_x = left_x + element.getSize().getWidth();
         int upper_y = element.getLocation().getY();
         int lower_y = upper_y + element.getSize().getHeight();
         int middle_y = (upper_y + lower_y) / 2;
-
-        TouchAction action = new TouchAction (driver);
-        action
-                .press(right_x, middle_y)
-                .waitAction(300)
-                .moveTo(left_x, middle_y)
-                .release()
-                .perform();
+        TouchAction action = new TouchAction(driver);
+        action.
+                press(PointOption.point(right_x, middle_y)).
+                waitAction(WaitOptions.waitOptions(Duration.ofMillis(300))).
+                moveTo(PointOption.point(left_x, middle_y)).
+                release().
+                perform();
     }
 }
