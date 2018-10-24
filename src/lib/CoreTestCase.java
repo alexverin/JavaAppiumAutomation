678dace2d4a332866b6ccbdaef7bf98844681fcc
Aleksey Verin <aleksey.verin@gismart.com>
2018-10-24 12:01:49 +0300

    6.01. Создание метода Platform и подключение skip

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,28 +1,23 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
 
-    private static final String PLATFORM_IOS = "ios";
-    private static final String PLATFORM_ANDROID = "android";
-    private static final String AppiumURL = "http://127.0.0.1:4723/wd/hub";
-
     protected AppiumDriver driver;
+    protected Platform Platform;
+
 
     @Override
-    public void setUp() throws Exception {
+    protected void setUp() throws Exception {
 
         super.setUp();
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-                driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
         this.rotateScreenPortrait();
 
     }
@@ -47,30 +42,5 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(Duration.ofMillis(seconds));
     }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception
-    {
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        if(platform.equals(PLATFORM_ANDROID))
-        {
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidDevice");
-            capabilities.setCapability("platformVersion", "8.0");
-            capabilities.setCapability("AutomationName", "Appium");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("app", "Users/alekseyverin/JAA_MY/apks/org.wikipedia.apk");
-        }
-        else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone X");
-            capabilities.setCapability("platformVersion", "12.0");
-            capabilities.setCapability("app", "Users/alekseyverin/JAA_MY/apks/Wikipedia.app");
-        }
-        else {
-            throw new Exception("Can't get run platform from env variable. Platform value " + platform);
-        }
-        return capabilities;
-    }
 }
--- src/tests/getStartedCase.java
+++ src/tests/getStartedCase.java
@@ -1,14 +1,18 @@
-package tests.iOS;
+package tests;
 
-import lib.IOSTestCase;
+import lib.CoreTestCase;
 import lib.ui.WelcomePageObject;
 import org.junit.Test;
 
-public class getStartedCase extends IOSTestCase {
+public class getStartedCase extends CoreTestCase {
 
     @Test
     public void testPassThroughWelcome()
     {
+        if (this.Platform.isAndroid())
+        {
+            return;
+        }
         WelcomePageObject WelcomePageObject = new WelcomePageObject(driver);
         WelcomePageObject.waitForLearnMoreLink();
         WelcomePageObject.clickNextButton();
